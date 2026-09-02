c85f228b92c9f93dfffbed0fbe3c97d93681d785
Sean O'Reilly (JHU) <152217304+seanr87@users.noreply.github.com>
2026-09-02 09:11:36 -0400

    Minor edit to CreateStrategusAnalysisSpecifications.R

--- scriptsForStudyDesigner/CreateStrategusAnalysisSpecifications.R
+++ scriptsForStudyDesigner/CreateStrategusAnalysisSpecifications.R
@@ -7,6 +7,7 @@
 # https://ohdsi.github.io/Strategus/reference/index.html#omop-cdm-hades-modules.
 # This help page also contains links to the corresponding HADES package that
 # further details.
+MINOR EDIT
 # ##############################################################################
 library(Strategus)
 library(tibble)
